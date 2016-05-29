fa4a79dc001756b84350e4e452a77a45f2a2603c
Zt Wang <mymisting@gmail.com>
2016-05-29 18:10:06 +0800

    Add RT project

---
+++ RegionTrigger/RegionTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariaApi.Server;
+using Terraria;
+using TShockAPI;
+
+namespace RegionTrigger
+{
+    public class RegionTrigger:TerrariaPlugin {
+        public override string Name => "RegionEvent";
+        public override string Author => "MistZZT";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+        public override string Description => "Perform actions in regions where players are active.";
+
+
+        public RegionTrigger(Main game) : base(game) { }
+        public override void Initialize() {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing) {
+            base.Dispose(disposing);
+        }
+    }
+}
